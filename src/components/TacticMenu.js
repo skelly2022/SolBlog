e896d9371d181cc4b8aaf05d8524f87bad25d86e
Jay100-coder <jerganhector@gmail.com>
2022-04-20 14:27:34 -0700

    removed card

--- src/components/TacticMenu.js
+++ src/components/TacticMenu.js
@@ -15,25 +15,6 @@ function TacticMenu() {
     >
       <div className="Intro">
         <h1 className="introText">Puzzle Rush</h1>
-        <Card className="cardShadow">
-          <ListGroup>
-            <ListGroup.Item>
-              <h3>Rules</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <h4>
-                You only get one chance, the game immediately ends once you get
-                one incorrect.
-              </h4>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <h4>Solve as many as you can</h4>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <h4>You only have 5 mins.</h4>
-            </ListGroup.Item>
-          </ListGroup>
-        </Card>
         <Form.Group
           style={{ width: "60%", marginTop: "20px" }}
           className="mb-3"
