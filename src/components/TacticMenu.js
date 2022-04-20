394902d8467c9d680f48b8d33aa1b4fdad8b3ea2
Jay100-coder <jerganhector@gmail.com>
2022-04-20 14:33:24 -0700

    minor changes

--- src/components/TacticMenu.js
+++ src/components/TacticMenu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form, Card, ListGroup } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function TacticMenu() {
